0c26eddf9edee4f03786de0ce83f1871b7c7448e
Hossein Gorji <hoseingorji1383@gmail.com>
2026-09-06 03:45:22 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        Map<Character , Integer> freq = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            freq.merge(s.charAt(i) , 1 , Integer::sum);
+        }
+
+        for(int j = 0; j < t.length(); j++){
+            freq.merge(t.charAt(j) , -1 , Integer::sum);
+        }
+
+        for(int count : freq.values()){
+            if(count != 0){
+                return false;
+            }
+        }
+        return true;
+
+
+
+        
+
+    }
+}
