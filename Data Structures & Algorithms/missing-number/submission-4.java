2c82a421bba4cbf39dd54bb841813d2d62f4454b
Hossein Gorji <hoseingorji1383@gmail.com>
2026-09-19 00:53:35 -0400

    Add: missing-number - submission-4

---
+++ Data Structures & Algorithms/missing-number/submission-4.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        
+        int missing = -1;
+        if(nums[nums.length - 1] != nums.length){
+            return nums.length;
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != i){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
